b7de0d8faca60404a9fedfcd8de54a3f3b943804
Martin Bram Moreinis <martin.moreinis@schoolsofwestfield.org>
2026-01-03 11:35:11 -0500

    Update NPC.js

--- NPC.js
+++ NPC.js
@@ -1,1 +1,19 @@
-/* store NPC constructor and all NPCs (including monsters) here. */
+/* store NPC constructor and all NPCs (including subclass monsters) here. */
+
+const monsters = [
+  {
+    name: "slime",
+    level: 2,
+    health: 15
+  },
+  {
+    name: "fanged beast",
+    level: 8,
+    health: 60
+  },
+  {
+    name: "dragon",
+    level: 20,
+    health: 300
+  }
+];
