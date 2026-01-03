9ab6022dd58ea9c82f64e2d58bff89bc2b0cf76d
Martin Bram Moreinis <martin.moreinis@schoolsofwestfield.org>
2026-01-03 11:34:21 -0500

    Create Object.js

---
+++ Object.js
@@ -0,0 +1,20 @@
+/* Constructor and methods for objects and weapons, subclass of objects */
+
+const allWeapons = [
+	{
+		name: "stick",
+		power: 5
+	},
+	{
+		name: "dagger",
+		power: 30
+	},
+	{
+		name: "claw hammer",
+		power: 50
+	},
+	{
+		name: "sword",
+		power: 100
+	}
+];
